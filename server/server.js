b4ee09375639c72e3f4c94ce4911fe7f75d76150
IvanZhovnych <izhovnych@optimiz.ca>
2024-04-04 19:05:38 -0500

    Updated path to index.html

--- server/server.js
+++ server/server.js
@@ -54,13 +54,13 @@ app.post('/send-email', (req, res) => {
 const __filename = fileURLToPath(import.meta.url);
 const __dirname = dirname(__filename);
 
-// Serve static files from the React app
-app.use(express.static(path.join(__dirname, 'client/build')));
+// Serve static files from the root directory
+app.use(express.static(__dirname));
 
 // The "catchall" handler: for any request that doesn't
 // match one above, send back React's index.html file.
 app.get('*', (req, res) => {
-  res.sendFile(path.join(__dirname, 'client/build', 'index.html'));
+  res.sendFile(path.join(__dirname, 'index.html'));
 });
 
 const PORT = process.env.PORT || 3001;
